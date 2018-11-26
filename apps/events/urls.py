a8ca7ff017da6419be6c7842531fe46bc70a36b9
Catalin Coroeanu <catalin.coroeanu@icloud.com>
2018-11-26 23:00:22 +0100

    Cleaned up urls

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,8 +6,6 @@ from apps.events import views
 urlpatterns = [
     url(r'^create/(?P<calendar_slug>[-\w]+)/$', views.create_or_edit_event,
         name='openduty.events.create_or_edit_event'),
-    # url(r'^create/(?P<calendar_slug>[-\w]+)/$', views.CreateUpdateEvent.as_view(),
-    #     name='openduty.events.create_or_edit_event'),
     url(r'^edit/(?P<calendar_slug>[-\w]+)/(?P<event_id>\d+)/$', views.create_or_edit_event, name='edit_event'),
     url(r'^destroy/(?P<calendar_slug>[-\w]+)/(?P<event_id>\d+)/$', views.destroy_event, name='destroy_event'),
 ]
